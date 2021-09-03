1bf7b7e8b8f1b40de61f8704947b5a528693f305
emeu17 <emeu17@student.bth.se>
2021-09-03 13:08:01 +0100

    Moved toolbar to top of page, using state lifting to save

--- my-editor/src/App.js
+++ my-editor/src/App.js
@@ -4,59 +4,75 @@ import "trix/dist/trix";
 import React, { Component } from 'react';
 import { TrixEditor } from "react-trix";
 
-class Editor extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          editorTxt: ""
-      };
-  }
+class Toolbar extends React.Component {
+    handleClick = () => {
+        // when Save-button is clicked, print editors text to console
+        console.log(this.props.editorTxt);
+    }
+    render() {
+        return (
+            <div className="Toolbar">
+                <button className="Save-button" onClick={() => this.handleClick()}> Save </button>
+            </div>
+        )
+    }
+}
 
-  handleEditorReady(editor) {
+class Editor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleEditorReady(editor) {
     // this is a reference back to the editor if you want to
     // do editing programatically
-    editor.insertString("editor is ready");
-  }
+        editor.insertString("editor is ready");
+    }
 
-  handleChange = (html, text) => {
-    // html is the new html content
-    // text is the new text content
+    handleChange(html, text) {
+        this.props.handleChange(html, text);
+    }
 
-    this.setState(() => ({
-      editorTxt: text,
-    }));
-
-  }
-
-  handleClick = () => {
-      // console.log(this.state.editorTxt);
-      console.log(this.state.editorTxt);
-  }
-
-  render() {
-    return (
-        <div>
-          <button className="Save-button" onClick={() => this.handleClick()}> Save </button>
-          <TrixEditor className="Trix" onChange={this.handleChange} onEditorReady={this.handleEditorReady} />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <TrixEditor className="Trix" onChange={this.handleChange} onEditorReady={this.handleEditorReady} />
+            </div>
+        );
+    }
 }
 
 class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My Editor
-        </p>
-      </header>
-      <Editor />
-    </div>
-    );
-  }
+    //App contains editors text saved as state variable
+    constructor(props) {
+        super(props);
+        // this.handleChange() = this.handleChange.bind(this);
+        this.state = {
+            editorTxt: ""
+        };
+    }
+    //handle change of editors text
+    handleChange = (html, text) => {
+      // html is the new html content
+      // text is the new text content
+      this.setState(() => ({
+        editorTxt: text,
+      }));
+    }
+    render() {
+        return (
+            <div className="App">
+                <Toolbar editorTxt={this.state.editorTxt}/>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        My Editor
+                    </p>
+                </header>
+                <Editor editorTxt={this.state.editorTxt} handleChange={this.handleChange}/>
+            </div>
+        );
+    }
 }
 
 export default App;
